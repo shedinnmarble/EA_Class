6d512bc21905df1e9aedd97b9f07f13ccc827444
Dewei Xiang <yafengxiang@163.com>
2016-09-20 16:19:45 -0500

    add lab9

---
+++ exercise09_2-receiver/src/main/java/cs544/exercise9_2/receiver/MDBPojo.java
@@ -0,0 +1,19 @@
+package cs544.exercise9_2.receiver;
+
+import javax.jms.*;
+
+
+public class MDBPojo implements MessageListener {
+
+    public void onMessage(Message message){
+    	ObjectMessage objmessage = (ObjectMessage)message;
+    	try {
+			Person person = (Person)objmessage.getObject();
+			System.out.println("MDBPojo receives message with person object : " + person.getFirstName()+" "+person.getLastName());
+		} catch (JMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+}
+
